5c7f14b70a5edc0b2d61c534d026451f6abe7d22
hiqqup <71655937+Hiqqup@users.noreply.github.com>
2025-10-29 12:09:08 +0100

    roating the model with mouse

--- src/main.cpp
+++ src/main.cpp
@@ -14,11 +14,15 @@
 #include "Mesh3d.h"
 
 
-glm::mat4 calculateMvpMatrix(glm::vec3 position,  glm::vec3 scale, float zRotationInDegrees) {
+glm::mat4 calculateMvpMatrix(glm::vec3 position,  glm::vec3 scale, glm::vec3 rotation) {
     glm::mat4 model = glm::mat4(1.0f);
     model = glm::translate(model, position);
     model = glm::scale(model, scale);
-    model = glm::rotate(model, glm::radians(zRotationInDegrees), glm::vec3(0.0f, 0.0f, 1.0f));
+
+    model = glm::rotate(model, glm::radians(rotation.x), glm::vec3(1.0f, 0.0f, 0.0f));
+    model = glm::rotate(model, glm::radians(rotation.y), glm::vec3(0.0f, 1.0f, 0.0f));
+    model = glm::rotate(model, glm::radians(rotation.z), glm::vec3(0.0f, 0.0f, 1.0f));
+
 
     glm::mat4 view = glm::lookAt(glm::vec3(0.0, 0.0, 90.0f),
                                  glm::vec3(0.0f, 0.0f, 0.0),
@@ -28,27 +32,56 @@ glm::mat4 calculateMvpMatrix(glm::vec3 position,  glm::vec3 scale, float zRotati
 
     return projection * view * model;
 }
-glm::mat4 calculateModelToWorldNormal(glm::vec3 position, glm::vec3 scale, float zRotation) {
+glm::mat4 calculateModelToWorldNormal(glm::vec3 position, glm::vec3 scale, glm::vec3 rotation) {
     glm::mat4 model = glm::mat4(1.0f);
     model = glm::translate(model, position);
     model = glm::scale(model, scale);
-    model = glm::rotate(model, glm::radians(zRotation), glm::vec3(0.0f, 0.0f, 1.0f));
+
+    model = glm::rotate(model, glm::radians(rotation.x), glm::vec3(1.0f, 0.0f, 0.0f));
+    model = glm::rotate(model, glm::radians(rotation.y), glm::vec3(0.0f, 1.0f, 0.0f));
+    model = glm::rotate(model, glm::radians(rotation.z), glm::vec3(0.0f, 0.0f, 1.0f));
+
     return glm::transpose(glm::inverse(model));
 }
 
-glm::vec3 position = glm::vec3(0.0f);
-glm::vec3 scale = glm::vec3(1.0f);
-glm::vec4 color =  glm::vec4(0.4f);
-float rotation = 0.0f;
+Window window;
+glm::vec2 getMousePosition() {
+    double xpos, ypos;
+    glfwGetCursorPos(window.window, &xpos, &ypos);
+    return {xpos, ypos};
+}
+
+auto position = glm::vec3(0.0f);
+auto scale = glm::vec3(1.0f);
+auto color =  glm::vec4(0.4f);
+auto rotation = glm::vec3(0.0f);
+
+auto mousePosition = getMousePosition();
+
+void processInput() {
+    auto newMousePosition = getMousePosition();
+    glm::vec2 deltaMousePosition = mousePosition - newMousePosition;
+    mousePosition = newMousePosition;
+
+    int state = glfwGetMouseButton(window.window, GLFW_MOUSE_BUTTON_LEFT);
+    if (state == GLFW_PRESS)
+    {
+        rotation.y -= deltaMousePosition.x;
+        rotation.x -= deltaMousePosition.y;
+    }
+
+}
 
 int main() {
 
-    Window window;
-    Mesh3d mesh = Mesh3d("models/spaceship.glb") ;
+    Mesh3d mesh = Mesh3d("models/monkey.glb") ;
     Shader shader;
     glEnable(GL_DEPTH_TEST);
 
     while(!glfwWindowShouldClose(window.window)) {
+
+        processInput();
+
         glClearColor(0.172f, 0.243f, 0.313f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
